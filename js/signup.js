69e2934d4de67796e72e67342c8a9f2bea28475e
ocaioaugusto <caioag42@gmail.com>
2019-05-17 00:19:08 -0300

    att validation and sirius

---
+++ js/signup.js
@@ -0,0 +1,29 @@
+﻿
+checkPassword = (form) => { 
+    password = form.password.value; 
+    confirmpassword = form.confirmpassword.value; 
+
+    if (password == '') 
+        alert ("Por favor, digite uma senha"); 
+    
+    else if (confirmpassword == '') 
+        alert ("Por favor, confirme sua senha"); 
+            
+    else if (password != confirmpassword) { 
+        alert ("\nAs senhas não combinam. Tente novamente...") 
+        return false; 
+    } 
+    else{  
+        return true; 
+    } 
+},
+
+emailIsValid = (email) => {
+var reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+
+if (reg.test(email.value) == false) { 
+    alert ("Email inválido");
+        return false;
+}
+    return true;
+}
